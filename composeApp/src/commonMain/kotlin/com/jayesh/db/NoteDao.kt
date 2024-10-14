fce84b4aa319f0efa5f53f8a069f9b207aa71fc0
Jayesh Patel <pateljayesh3737@gmail.com>
2024-10-14 12:57:57 +0530

    add NoteDao

---
+++ composeApp/src/commonMain/kotlin/com/jayesh/db/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.jayesh.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Delete
+import com.jayesh.data.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes")
+    suspend fun getAllNotes(): List<Note>
+}
