2eab306895a55a1368ab7bfe71238fa569cddf6e
Jayesh Patel <pateljayesh3737@gmail.com>
2024-10-14 12:57:56 +0530

    Add background color to Note

--- composeApp/src/commonMain/kotlin/com/jayesh/data/Note.kt
+++ composeApp/src/commonMain/kotlin/com/jayesh/data/Note.kt
@@ -8,6 +8,7 @@ data class Note(
     val content: String,       // Content/body of the note
     val createdAt: LocalDateTime,  // Date and time when the note was created
     val updatedAt: LocalDateTime,  // Date and time when the note was last updated
+    val backgroundColor: Long = 0xffe5dcc5,
     val isSynced: Boolean = false  // Flag to check if the note is synced with the server
 )
 
@@ -19,13 +20,15 @@ val sampleNotes = listOf(
         title = "Sample Note 1",
         content = "This is the content of sample note 1.",
         createdAt = today,
-        updatedAt = today
+        updatedAt = today,
+        backgroundColor = 0xffe5dcc5
     ),
     Note(
         id = "2",
         title = "Sample Note 2",
         content = "This is the content of sample note 2.",
         createdAt = today,
-        updatedAt = today
+        updatedAt = today,
+        backgroundColor = 0xff85c7f2
     )
 )
--- composeApp/src/commonMain/kotlin/com/jayesh/ui/HomePage.kt
+++ composeApp/src/commonMain/kotlin/com/jayesh/ui/HomePage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.jayesh.data.Note
 
@@ -40,7 +41,8 @@ fun NoteItem(note: Note) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-        elevation = CardDefaults.elevatedCardElevation()
+        elevation = CardDefaults.elevatedCardElevation(),
+        colors = CardDefaults.cardColors(containerColor = Color(note.backgroundColor))
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
