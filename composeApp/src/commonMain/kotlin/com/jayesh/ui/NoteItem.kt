c98edf79fb44026c1e36b00c7bc78d97cfb42420
Jayesh Patel <pateljayesh3737@gmail.com>
2024-10-17 10:02:46 +0530

    fix NoteItem theme based on selected them option

--- composeApp/src/commonMain/kotlin/com/jayesh/ui/NoteItem.kt
+++ composeApp/src/commonMain/kotlin/com/jayesh/ui/NoteItem.kt
@@ -1,37 +1,36 @@
 package com.jayesh.ui
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.jayesh.data.Note
-import com.jayesh.ui.theme.getColorScheme
+import com.jayesh.ui.theme.AppTheme
 
 @Composable
 fun NoteItem(
     note: Note,
     onNoteItemClicked: () -> Unit = {}
 ) {
-    ElevatedCard(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        colors = CardDefaults.elevatedCardColors(
-            containerColor = note.appThemeOption.getColorScheme(isSystemInDarkTheme()).primaryContainer
-        ),
-        onClick = { onNoteItemClicked() }
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
+    AppTheme(themeOption = note.appThemeOption) {
+        ElevatedCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            onClick = { onNoteItemClicked() }
         ) {
-            Text(text = note.title, style = MaterialTheme.typography.titleMedium)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = note.content, style = MaterialTheme.typography.bodyMedium)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = "Created: ${note.createdAt}", style = MaterialTheme.typography.bodySmall)
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = note.title, style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = note.content, style = MaterialTheme.typography.bodyMedium)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "Created: ${note.createdAt}", style = MaterialTheme.typography.bodySmall)
+            }
         }
     }
 }
